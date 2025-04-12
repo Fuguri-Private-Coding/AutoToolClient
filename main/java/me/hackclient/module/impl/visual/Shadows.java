c680589ef9bb18faed0a245f7e85f773881a0209
Deathlksr <92933576+VerLouF@users.noreply.github.com>
2025-04-12 18:18:35 +0500

    Update Shadows.java

--- main/java/me/hackclient/module/impl/visual/Shadows.java
+++ main/java/me/hackclient/module/impl/visual/Shadows.java
@@ -31,13 +31,13 @@ public class Shadows extends Module {
 
     public IntegerSetting radius = new IntegerSetting("Radius", this, 1, 15, 6);
     public FloatSetting brightness = new FloatSetting("Brightness", this, 0,2,1,0.1f);
-    public FloatSetting horizontal1Compress = new FloatSetting("Horizontal1Compress", this, 0, 10, 1, 0.1f);
-    public FloatSetting vertical1Compress = new FloatSetting("Vertical1Compress", this, 0, 10, 0, 0.1f);
-    public FloatSetting horizontal2Compress = new FloatSetting("Horizontal2Compress", this, 0, 10, 0, 0.1f);
-    public FloatSetting vertical2Compress = new FloatSetting("Vertical2Compress", this, 0, 10, 1, 0.1f);
+    public FloatSetting horizontal1Compress = new FloatSetting("Horizontal1Compress", this, -10, 10, 2, 0.1f);
+    public FloatSetting vertical1Compress = new FloatSetting("Vertical1Compress", this, -10, 10, 0, 0.1f);
+    public FloatSetting horizontal2Compress = new FloatSetting("Horizontal2Compress", this, -10, 10, 0, 0.1f);
+    public FloatSetting vertical2Compress = new FloatSetting("Vertical2Compress", this, -10, 10, 2, 0.1f);
 
     public BooleanSetting fade = new BooleanSetting("Fade", this, false);
     public ColorSetting color = new ColorSetting("ShadowColor", this, 0,0,0,1);
     public ColorSetting twoColor = new ColorSetting("TwoShadowColor", this, fade::isToggled, 0,0,0,1);
-    public FloatSetting speed = new FloatSetting("Speed", this, fade::isToggled,0.1f, 10, 1, 0.1f);
+    public FloatSetting speed = new FloatSetting("Speed", this, fade::isToggled,0.1f, 20, 1, 0.1f);
 }
