00314c494de6486cce896750e7b8de41cc24b1cf
dicves1234 <timt79956@gmail.com>
2025-05-08 21:48:09 +0400

    Create TimerRangeV2.java

---
+++ main/java/me/hackclient/module/impl/combat/TimerRangeV2.java
@@ -0,0 +1,81 @@
+package me.hackclient.module.impl.combat;
+
+import me.hackclient.Client;
+import me.hackclient.event.Event;
+import me.hackclient.event.EventTarget;
+import me.hackclient.event.events.TickEvent;
+import me.hackclient.module.Category;
+import me.hackclient.module.Module;
+import me.hackclient.module.ModuleInfo;
+import me.hackclient.settings.impl.IntegerSetting;
+import me.hackclient.utils.predict.SimulatedPlayer;
+import me.hackclient.utils.raytrace.RayTraceUtils;
+import me.hackclient.utils.rotation.Rotation;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.MovingObjectPosition;
+
+import java.io.IOException;
+
+@ModuleInfo(name = "TimerRangeV2", category = Category.COMBAT)
+public class TimerRangeV2 extends Module {
+
+    private IntegerSetting ticks = new IntegerSetting("Ticks", this, 1, 10, 3);
+    private IntegerSetting hurtTime = new IntegerSetting("HurtTime", this, 0, 10, 3);
+
+    private int balance;
+    private boolean teleporting;
+
+    @EventTarget
+    public void onEvent(Event event) {
+        if (event instanceof TickEvent e && !teleporting) {
+            if (balance > 0) {
+                e.cancel();
+                balance--;
+                return;
+            }
+
+            EntityLivingBase target = Client.INSTANCE.getCombatManager().getTarget();
+
+            if (target == null || target.hurtTime > hurtTime.getValue()) {
+                return;
+            }
+
+            if (mc.objectMouseOver != null && mc.objectMouseOver.entityHit == target) {
+                return;
+            }
+
+            SimulatedPlayer simulatedPlayer = SimulatedPlayer.fromClientPlayer(mc.thePlayer.movementInput);
+            int predictedTicks = 0;
+
+            for (int tick = 1; tick <= ticks.getValue(); tick++) {
+                simulatedPlayer.tick();
+                MovingObjectPosition mouse = RayTraceUtils.rayTrace(
+                        simulatedPlayer.getPosEyes(),
+                        3d,
+                        4.5d,
+                        Rotation.getServerRotation()
+                );
+
+                if (mouse != null && mouse.entityHit == target) {
+                    predictedTicks = tick;
+                    break;
+                }
+            }
+
+            if (predictedTicks == 0) {
+                return;
+            }
+
+            teleporting = true;
+            for (int i = 0; i < predictedTicks; i++) {
+                try {
+                    mc.runTick();
+                } catch (IOException _) {
+                }
+            }
+            teleporting = false;
+
+            balance = predictedTicks;
+        }
+    }
+}
