94eb54cc7837521fcd41955e176cbf6777184633
dicves1234 <timt79956@gmail.com>
2025-05-14 03:53:45 +0400

    hwid system igrat

---
+++ main/java/me/hackclient/utils/hwid/HWIDUtils.java
@@ -0,0 +1,85 @@
+package me.hackclient.utils.hwid;
+
+import me.hackclient.Client;
+import me.hackclient.utils.profile.Profile;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+public class HWIDUtils {
+    private static final String Database_URL = "https://pastebin.com/raw/32VeuV38";
+
+    public static void check() {
+        String hwid = generateHWID();
+        System.out.println("current hwid " + hwid);
+        if (IsWhiteList(hwid)) {
+            System.out.println("Logged as " + Client.INSTANCE.getProfile());
+        } else {
+            System.exit(0);
+        }
+    }
+
+    private static String getSerialNumber() {
+        String serialNumber = "";
+        try {
+            Process process = Runtime.getRuntime().exec(new String[] { "wmic", "diskdrive", "get", "serialnumber" });
+            process.getOutputStream().close();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.trim().startsWith("SerialNumber")) {
+                    serialNumber = line.split(" ")[0];
+                    break;
+                }
+            }
+            reader.close();
+            process.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return serialNumber;
+    }
+
+    public static String generateHWID() {
+        String input = "";
+        input += getSerialNumber();
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hash) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static boolean IsWhiteList(String hwid) {
+        try {
+            URL url = new URL(Database_URL);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] args = line.trim().split(":");
+                String hwidLine = args[0];
+                String nameLine = args[1];
+                String roleLine = args[2];
+                if (hwidLine.equals(hwid)) {
+                    reader.close();
+                    Client.INSTANCE.setProfile(new Profile(nameLine, roleLine));
+                    return true;
+                }
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
