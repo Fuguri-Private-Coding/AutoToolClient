4810e39a573b545b60401ee26275398db28f0dd3
dicves1234 <timt79956@gmail.com>
2025-05-15 13:07:55 +0400

    Update IRCModule.java

--- main/java/me/hackclient/module/impl/client/IRCModule.java
+++ main/java/me/hackclient/module/impl/client/IRCModule.java
@@ -4,6 +4,7 @@ import me.hackclient.Client;
 import me.hackclient.event.Event;
 import me.hackclient.event.EventTarget;
 import me.hackclient.event.events.TickEvent;
+import me.hackclient.event.events.WorldChangeEvent;
 import me.hackclient.module.Category;
 import me.hackclient.module.Module;
 import me.hackclient.module.ModuleInfo;
@@ -22,15 +23,15 @@ public class IRCModule extends Module {
 
     @Override
     public void onDisable() {
-        if (myId != -1) {
-            Client.INSTANCE.getIrc().getServerChannel().deleteMessageById(myId).queue();
-        }
-        myId = -1;
+        if (myId != -1) Client.INSTANCE.getIrc().getServerChannel().deleteMessageById(myId).queue(_ -> {
+            myId = -1;
+        });
     }
 
     @EventTarget
     public void onEvent(Event event) {
-        if (event instanceof TickEvent && mc.thePlayer.ticksExisted % 200 == 0) {
+        if (event instanceof WorldChangeEvent) {
+            // Поиск других юзеров
             new Thread(() -> {
                 usersOnline.clear();
                 for (Message message : Client.INSTANCE.getIrc().getServerChannel().getIterableHistory().stream().toList()) {
@@ -45,19 +46,21 @@ public class IRCModule extends Module {
                 }
             }).start();
 
+            // Отправка самого себя на сервер
             new Thread(() -> {
                 if (myId == -1 || !currentInfo.equalsIgnoreCase(mc.getSession().getUsername() + Client.INSTANCE.getProfile())) {
-                if (myId != -1) {
-                    Client.INSTANCE.getIrc().getServerChannel().deleteMessageById(myId).queue();
+                    if (myId != -1) {
+                        ClientUtils.chatLog("удалил прошлое сообщение " + myId);
+                        Client.INSTANCE.getIrc().getServerChannel().deleteMessageById(myId).queue();
+                    }
+                    ClientUtils.chatLog("Отправил сообщение " + mc.getSession().getUsername() + " " + Client.INSTANCE.getProfile());
+                    Client.INSTANCE.getIrc().getServerChannel().sendMessage(
+                            mc.getSession().getUsername() + " " + Client.INSTANCE.getProfile()
+                    ).queue(sendMessage -> {
+                        currentInfo = mc.getSession().getUsername() + Client.INSTANCE.getProfile();
+                        myId = sendMessage.getIdLong();
+                    });
                 }
-                Client.INSTANCE.getIrc().getServerChannel().sendMessage(
-                        mc.getSession().getUsername() + " " + Client.INSTANCE.getProfile()
-                ).queue(sendMessage -> {
-                    currentInfo = mc.getSession().getUsername() + Client.INSTANCE.getProfile();
-                    myId = sendMessage.getIdLong();
-                });
-
-            }
             }).start();
         }
     }
