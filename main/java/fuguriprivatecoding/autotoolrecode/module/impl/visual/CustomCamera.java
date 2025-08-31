46dfc995b1b3db5a2a7a982cda1d5f9ee8171df8
dicves1234 <timt79956@gmail.com>
2025-08-31 22:03:11 +0400

    GTA 5 View

--- main/java/fuguriprivatecoding/autotoolrecode/module/impl/visual/CustomCamera.java
+++ main/java/fuguriprivatecoding/autotoolrecode/module/impl/visual/CustomCamera.java
@@ -10,4 +10,6 @@ import fuguriprivatecoding.autotoolrecode.settings.impl.FloatSetting;
 public class CustomCamera extends Module {
     public FloatSetting cameraDistance = new FloatSetting("CameraDistance",this, 1f, 5f, 1f, 0.1f) {};
     public CheckBox cameraClip = new CheckBox("CameraClip", this, true);
+    public final CheckBox gta5 = new CheckBox("GTA5", this, false);
+    public FloatSetting translate = new FloatSetting("Translate", this, gta5::isToggled, 0, 2, 0.75f, 0.01f);
 }
