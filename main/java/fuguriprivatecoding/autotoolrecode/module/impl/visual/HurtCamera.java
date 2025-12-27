17ac62b31a5925e811d5d5a89fa0c0277f7d43e6
VerLouF <92933576+Deathlksr@users.noreply.github.com>
2025-12-27 20:38:53 +0500

    Пофиксел харткамера, теперь норм

--- main/java/fuguriprivatecoding/autotoolrecode/module/impl/visual/HurtCamera.java
+++ main/java/fuguriprivatecoding/autotoolrecode/module/impl/visual/HurtCamera.java
@@ -1,34 +1,12 @@
 package fuguriprivatecoding.autotoolrecode.module.impl.visual;
 
-import fuguriprivatecoding.autotoolrecode.event.Event;
-import fuguriprivatecoding.autotoolrecode.event.events.world.PacketEvent;
 import fuguriprivatecoding.autotoolrecode.module.Category;
 import fuguriprivatecoding.autotoolrecode.module.Module;
 import fuguriprivatecoding.autotoolrecode.module.ModuleInfo;
 import fuguriprivatecoding.autotoolrecode.setting.impl.FloatSetting;
-import net.minecraft.network.play.server.S12PacketEntityVelocity;
-import net.minecraft.util.MathHelper;
 
 @ModuleInfo(name = "HurtCamera", category = Category.VISUAL, description = "Позволяет изменить тряску при ударе.")
 public class HurtCamera extends Module {
 
     public FloatSetting strength = new FloatSetting("Strength", this, 0, 1, 0, 0.1f);
-
-    @Override
-    public void onEvent(Event event) {
-        if (event instanceof PacketEvent e &&
-            e.getPacket() instanceof S12PacketEntityVelocity s12 &&
-            s12.getId() == mc.thePlayer.getEntityId()) {
-            final double velocityX = s12.getMotionX() / 8000.0D;
-            final double velocityZ = s12.getMotionZ() / 8000.0D;
-
-            mc.thePlayer.attackedAtYaw = (float) (MathHelper.atan2(velocityX, velocityZ) * 180.0D / Math.PI - mc.thePlayer.rotationYaw);
-        }
-    }
-
-    @Override
-    public void onDisable() {
-        mc.thePlayer.attackedAtYaw = 0;
-    }
-
 }
--- main/java/net/minecraft/client/renderer/EntityRenderer.java
+++ main/java/net/minecraft/client/renderer/EntityRenderer.java
@@ -539,9 +539,11 @@ public class EntityRenderer implements IResourceManagerReloadListener {
             f = MathHelper.sin(f * f * f * f * (float) Math.PI);
             final HurtCamera hurtCamera = Modules.getModule(HurtCamera.class);
 
-            final float f2 = hurtCamera.isToggled() ? 0 : entitylivingbase.attackedAtYaw;
+            f = f * (hurtCamera.isToggled() ? hurtCamera.strength.getValue() : 1f);
+
+            final float f2 = entitylivingbase.attackedAtYaw;
             GlStateManager.rotate(-f2, 0.0F, 1.0F, 0.0F);
-            GlStateManager.rotate(-f * 14.0F, 0.0F, 0.0F, hurtCamera.isToggled() ? hurtCamera.strength.getValue() : 1.0F);
+            GlStateManager.rotate(-f * 14.0F, 0.0F, 0.0F, 1.0F);
             GlStateManager.rotate(f2, 0.0F, 1.0F, 0.0F);
         }
     }
