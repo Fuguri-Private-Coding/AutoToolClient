ae2f8cfbd6fa1d637cbf78b054f01802d3081b61
VerLouF <92933576+Deathlksr@users.noreply.github.com>
2026-02-03 23:56:40 +0500

    релиз

--- main/java/fuguriprivatecoding/autotoolrecode/Client.java
+++ main/java/fuguriprivatecoding/autotoolrecode/Client.java
@@ -53,7 +53,7 @@ public enum Client implements Imports, EventListener {
 	INST;
 
 	public final String CLIENT_NAME = "AutoTool";
-    public final ClientVersion CLIENT_VERSION = new ClientVersion(4, 5,4);
+    public final ClientVersion CLIENT_VERSION = new ClientVersion(4, 6,0);
 
     private final String RESOURCES_ID = "minecraft";
     private final String RESOURCES_CLIENT_ID = "autotool/";
