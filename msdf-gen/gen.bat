b1de07f4df380ddbfb06895ffcfb2e8e7d0d7976
dicves1234 <timt79956@gmail.com>
2026-02-18 17:34:05 +0400

    fonti

--- msdf-gen/gen.bat
+++ msdf-gen/gen.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set /p fontname="font file name (with file extension): "
+set /p fontfilename="font file name (with file extension): "
 cls
 
 set /p size="size: "
@@ -9,5 +9,5 @@ cls
 set /p pxrange="pxRange: "
 cls
 
-msdf.exe -font %fontname% -charset charset.txt -type mtsdf -format png -imageout atlas.png -json data.json -size %size% -square4 -pxrange %pxrange%
+msdf.exe -font %fontfilename% -charset charset.txt -type mtsdf -format png -imageout atlas.png -json data.json -size %size% -square4 -pxrange %pxrange%
 pause
